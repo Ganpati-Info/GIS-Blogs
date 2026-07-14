22137b2630adc8eb1e9d78ad467488184de27d28
Souvik <souvik.basak2404@gmail.com>
2026-07-14 18:16:24 +0530

    feat: add initial page components for Author, Authors, Blog, Category, and Search, along with index files for assets, blog, sections, and hooks

---
+++ src/app/authors/page.tsx
@@ -0,0 +1,3 @@
+export default function AuthorsPage() {
+  return <div>hello from that page</div>;
+}
---
+++ src/app/search/page.tsx
@@ -0,0 +1,3 @@
+export default function SearchPage() {
+  return <div>hello from that page</div>;
+}
