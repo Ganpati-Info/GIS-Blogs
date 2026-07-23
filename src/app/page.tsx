baa97689d103807ed9dbdd4b244c9b3ea975be03
Souvik <souvik.basak2404@gmail.com>
2026-07-23 16:31:08 +0530

    feat: add Icon component and refactor CategoryTabs to utilize it for category icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import HomePageClient from "@/components/home/HomePageClient";
 
 import { getPosts } from "@/services/post.service";
 import { getCategories } from "@/services/category.service";
+export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
   const posts = await getPosts();
@@ -10,6 +11,13 @@ export default async function HomePage() {
   const featuredPost = posts.find((post) => post.featured) ?? posts[0];
 
   const popularPosts = posts.filter((post) => post.popular);
+
+console.log(
+  posts.map((p) => ({
+    title: p.title,
+    featured: p.featured,
+  }))
+);
 
   return (
     <HomePageClient
