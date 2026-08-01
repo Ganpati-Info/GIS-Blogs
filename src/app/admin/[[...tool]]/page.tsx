c1acc041a9c7bbd3d7ce7c1bb1ddef1ae799dc02
Souvik <souvik.basak2404@gmail.com>
2026-08-01 15:50:54 +0530

    feat: implement Studio component and update page routing for admin tools

--- src/app/admin/[[...tool]]/page.tsx
+++ src/app/admin/[[...tool]]/page.tsx
@@ -1,8 +1,18 @@
-"use client";
+import { redirect } from "next/navigation";
+import Studio from "./Studio";
 
-import { NextStudio } from "next-sanity/studio";
-import config from "../../../../sanity.config";
+interface Props {
+  params: Promise<{
+    tool?: string[];
+  }>;
+}
 
-export default function StudioPage() {
-  return <NextStudio config={config} />;
+export default async function Page({ params }: Props) {
+  const { tool } = await params;
+
+  if (!tool?.length) {
+    redirect("/admin/structure/dashboard");
+  }
+
+  return <Studio />;
 }
